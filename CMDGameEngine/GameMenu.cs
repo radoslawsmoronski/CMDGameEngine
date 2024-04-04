f9590613cc0420382665350f1ea858af784da410
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-04-04 17:30:59 +0200

    Paste code from my older project - ConsoleGame

--- CMDGameEngine/GameMenu.cs
+++ CMDGameEngine/GameMenu.cs
@@ -2,6 +2,109 @@
 {
     public class GameMenu
     {
+        public class Menu
+        {
+            Game gameSession;
 
+            MenuOptions menuOptions = MenuOptions.NewGame;
+
+            public enum MenuOptions
+            {
+                NewGame,
+                Exit
+            }
+
+            public Menu()
+            {
+            }
+
+            public void Show()
+            {
+
+                Dictionary<MenuOptions, string> menuOptionsDictionary = new Dictionary<MenuOptions, string>();
+                menuOptionsDictionary.Add(MenuOptions.NewGame, "New Game");
+                menuOptionsDictionary.Add(MenuOptions.Exit, "Exit");
+
+                string header = GetHeader("Console Game");
+
+                while (true)
+                {
+                    Console.SetCursorPosition(0, 0);
+                    Console.Clear();
+
+                    string menuString = header + "\n\n";
+
+                    foreach (KeyValuePair<MenuOptions, string> pair in menuOptionsDictionary)
+                    {
+                        if (pair.Key == menuOptions) menuString += $"\n> {pair.Value} <\n\n";
+                        else menuString += $"  {pair.Value}  \n";
+                    }
+
+                    Console.WriteLine(menuString);
+
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.UpArrow: MoveMenuOptionsEnum(-1); break;
+                        case ConsoleKey.DownArrow: MoveMenuOptionsEnum(+1); break;
+                        case ConsoleKey.Enter: MenuAction(menuOptions); break;
+                        default: break;
+                    }
+                }
+
+            }
+
+            public void MoveMenuOptionsEnum(int i)
+            {
+                int numOptions = Enum.GetNames(typeof(MenuOptions)).Length;
+                menuOptions = (MenuOptions)(((int)menuOptions + i + numOptions) % numOptions);
+            }
+
+            public string GetHeader(string text)
+            {
+                string topFrame = "╔";
+                string mediumFrame = "║";
+                string lowFrame = "╚";
+
+                foreach (char letter in text)
+                {
+                    topFrame += "═";
+                    lowFrame += "═";
+                }
+
+                topFrame += "╗";
+                mediumFrame += $"{text}║";
+                lowFrame += "╝";
+
+                return $"{topFrame}\n{mediumFrame}\n{lowFrame}";
+            }
+
+            public void MenuAction(MenuOptions menuOption)
+            {
+                switch (menuOption)
+                {
+                    case MenuOptions.Exit: Environment.Exit(0); break;
+                    case MenuOptions.NewGame: NewGame(); break;
+                    default: break;
+                }
+            }
+
+            public void NewGame()
+            {
+                Console.Clear();
+
+                gameSession = new Game(100, 20);
+                gameSession.BackToMenu += BackToMenu;
+                gameSession.Run();
+            }
+
+            public void BackToMenu()
+            {
+                Console.Clear();
+                Show();
+                return;
+            }
+        }
     }
 }
