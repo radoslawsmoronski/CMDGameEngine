6dc11d91c0b924ec250ff0a4deb1e9f1e864d968
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-04-05 15:37:13 +0200

    Few changes in CMDSupportProject

--- CMDSupportProject/Program.cs
+++ CMDSupportProject/Program.cs
@@ -4,18 +4,24 @@ namespace CMDSupportProject
 {
     internal class Program
     {
+        static GameMenu gameMenu = new GameMenu("New Game", null);
         static void Main(string[] args)
         {
-            GameMenu gameMenu = new GameMenu("New Game", null);
-
             List<MenuOption> menuOptions = new List<MenuOption>();
 
-            menuOptions.Add(new MenuOption("Start Game", () => MenuOptionsMethods.ExitGame(gameMenu)));
+            menuOptions.Add(new MenuOption("Start Game", () => StartGame()));
+            menuOptions.Add(new MenuOption("Exit Game", () => MenuOptionsMethods.ExitGame(gameMenu)));
 
             gameMenu.AddMenuOptions(menuOptions);
 
 
             gameMenu.Show();
+        }
+
+        public static void StartGame()
+        {
+            gameMenu.CloseMenu();
+            Console.WriteLine("Hello!");
         }
 
     }
