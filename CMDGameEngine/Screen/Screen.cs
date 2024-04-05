4f2479878b674405c53ce9e74f24b3453a989cb9
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-04-05 18:28:08 +0200

    I Started work on Screen class. I created Screen dictionary, fundamental properies and contructor

---
+++ CMDGameEngine/Screen/Screen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace CMDGameEngine.Screen
+{
+    public class Screen
+    {
+        public int ScreenWidth { get; private set; }
+        public int ScreenHeight { get; private set; }
+
+        public Screen(int screenWidth = 20, int screenHeight = 20) 
+        {
+
+            ScreenWidth = screenWidth;
+            ScreenHeight = screenHeight;
+        }
+    }
+}
