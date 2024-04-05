740fc9ad0afa06e1bbc0f5f06f20948057126f7b
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-04-05 20:03:04 +0200

    I created GameObject class in GameObjects directory

---
+++ CMDGameEngine/GameObjects/Object.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMDGameEngine.GameObjects
+{
+    public class Object
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Object(int x, int y)
+        {
+            X = x;
+            Y = y;
+
+        }
+
+        public void Move(int x, int y)
+        {
+            X = X + x;
+            Y = Y + y;
+
+        }
+
+    }
+}
--- CMDSupportProject/Program.cs
+++ CMDSupportProject/Program.cs
@@ -25,7 +25,6 @@ namespace CMDSupportProject
 
 
             gameMenu.Show();
-
         }
 
         public static void StartGame()
