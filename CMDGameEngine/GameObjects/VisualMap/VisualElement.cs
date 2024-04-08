87ce0ead5ab648a7a7cde4bf1b352833dedfc0f7
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-04-08 15:45:20 +0200

    I created VisualMap dictionary in GameObjects, VisualElements.cs and VisualMap.cs

---
+++ CMDGameEngine/GameObjects/VisualMap/VisualElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This class is a part of the VisualMap, and containt char on specific position in the game.
+
+namespace CMDGameEngine.GameObjects.VisualMap
+{
+    public class VisualElement
+    {
+        public int XPosToVisualMap { get; private set; }
+        public int YPosToVisualMap { get; private set; }
+
+        public char Sign {  get; private set; }
+
+        public VisualElement(int x, int y, char sign)
+        {
+            XPosToVisualMap = x;
+            YPosToVisualMap = y;
+            Sign = sign;
+        }
+    }
+}
---
+++ CMDGameEngine/GameObjects/VisualMap/VisualMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Class to containt a visual part of an object.
+
+namespace CMDGameEngine.GameObjects.VisualMap
+{
+    public class VisualMap
+    {
+        List<VisualElement> visualElements = new List<VisualElement>();
+
+        public VisualMap(List<VisualElement> visualElements)
+        {
+            this.visualElements = visualElements;
+        }
+    }
+}
