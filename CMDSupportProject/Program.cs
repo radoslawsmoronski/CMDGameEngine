f6b8ad9e017b76c049b3a0da0a3a0e6907a13395
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-04-12 14:46:52 +0200

    I tested upgraded xml method

--- CMDSupportProject/Program.cs
+++ CMDSupportProject/Program.cs
@@ -12,10 +12,20 @@ namespace CMDSupportProject
 
     internal class Program
     {
+        static GameScreen gameScreen = new GameScreen(40, 10);
+
+
+        static string xml = "<objectVisualMap>\r\n  <element x=\"0\" y=\"2\" sign=\"(\" />\r\n  <element x=\"1\" y=\"1\" sign=\"(\" />\r\n  <element x=\"2\" y=\"0\" sign=\"_\" />\r\n  <element x=\"3\" y=\"0\" sign=\"_\" />\r\n  <element x=\"4\" y=\"0\" sign=\"_\" />\r\n  <element x=\"5\" y=\"1\" sign=\")\" />\r\n  <element x=\"4\" y=\"1\" sign=\"o\" />\r\n  <element x=\"2\" y=\"1\" sign=\"o\" />\r\n  <element x=\"3\" y=\"2\" sign=\"V\" />\r\n  <element x=\"6\" y=\"2\" sign=\")\" />\r\n  <element x=\"5\" y=\"3\" sign=\"-\" />\r\n  <element x=\"1\" y=\"3\" sign=\"-\" />\r\n  <element x=\"2\" y=\"3\" sign=\"m\" />\r\n  <element x=\"3\" y=\"3\" sign=\"-\" />\r\n  <element x=\"4\" y=\"3\" sign=\"m\" />\r\n</objectVisualMap>";
+
+
+        static VisualMap visualMap = new VisualMap(xml);
+        static GameObject gameObject = new GameObject(5, 5, visualMap);
 
         static void Main(string[] args)
         {
 
+
+            gameScreen.Show();
         }
 
       
